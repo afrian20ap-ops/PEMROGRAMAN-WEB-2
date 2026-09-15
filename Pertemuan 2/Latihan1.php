5e3e260d78e7b9efa503141a9f316b4e57e19ded
afrian20ap-ops <afrian20.ap@gmail.com>
2026-09-15 23:07:50 +0700

    Initial commit

---
+++ Pertemuan 2/Latihan1.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 1;
+$b = 2;
+
+$c = $a . $b;
+
+// Nilai $c menjadi 12
+$d = $c + 1;
+
+echo "Hasil d = " . $d . "<br>";
+
+$e = "Number";
+$f = $e . $d;
+
+echo "Hasil f = " . $f;
+
+?>
